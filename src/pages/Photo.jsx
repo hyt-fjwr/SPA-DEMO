4befd631795e47460e96d8573a87890b25532266
crow-editing <f.hayato0104@gmail.com>
2023-06-07 23:04:42 +0900

    Added photo from flickr

--- src/pages/Photo.jsx
+++ src/pages/Photo.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const Photo = () => {
   return (
-    <div>
-      <p>Photos</p>
+    <div className='flex justify-center mt-5'>
+      <div className='md:w-2/5 lg:max-w-lg w-2/4'>
+        <a data-flickr-embed="true" title="asakusa"><img src="https://live.staticflickr.com/65535/52610710948_45c55b189e_c.jpg" width="534" height="800" alt="asakusa"/></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>
+      </div>
     </div>
   )
 }
